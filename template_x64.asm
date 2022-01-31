7e3f56d25efba6073607775e6549ec8347993880
Alexey Butyugin <Alex02072002@yandex.ru>
2022-01-31 22:05:18 +0300

    Initial. Add templates

---
+++ template_x64.asm
@@ -0,0 +1,30 @@
+    section .data           ; сегмент инициализированных переменных
+ExitMsg db "Press Enter to Exit", 10 ; выводимое сообщение
+lenExit equ $-ExitMsg
+
+    section .bss            ; сегмент неинициализированных переменных
+InBuf   resb    10          ; буфер для вводимой строки
+lenIn   equ     $-InBuf    ; длина буфера для вводимой строки
+
+    section .text           ; сегмент кода
+    global _start
+
+_start:
+    ; write
+    mov     rax, 1        ; системная функция 1 (write)
+    mov     rdi, 1        ; дескриптор файла stdout=1
+    mov     rsi, ExitMsg  ; адрес выводимой строки
+    mov     rdx, lenExit  ; длина строки
+    syscall               ; вызов системной функции
+
+    ; read
+    mov     rax, 0        ; системная функция 0 (read)
+    mov     rdi, 0        ; дескриптор файла stdin=0
+    mov     rsi, InBuf    ; адрес вводимой строки
+    mov     rdx, lenIn    ; длина строки
+    syscall               ; вызов системной функции
+
+    ; exit
+    mov     rax, 60       ; системная функция 60 (exit)
+    xor     rdi, rdi      ; return code 0    
+    syscall               ; вызов системной функции
