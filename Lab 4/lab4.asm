ca4bdf65ede143c5b30f9765eab1178242fddb91
Alexey Butyugin <Alex02072002@yandex.ru>
2022-03-07 23:22:16 +0300

    Move operators into functions in Lab 4

--- Lab 4/lab4.asm
+++ Lab 4/lab4.asm
@@ -8,30 +8,6 @@
 
 %include "../lib64.asm"
 
-%macro write_string 2
-    ; Макрос, предназначенный для вывода сообщений
-    ; Аргументы:
-    ;   1 - Адрес строки
-    ;   2 - Длина строки
-    mov     rax, WRITE      ; системная функция
-    mov     rdi, STDOUT     ; дескриптор файла
-    mov     rsi, %1         ; адрес выводимой строки
-    mov     rdx, %2         ; длина строки
-    syscall                 ; вызов системной функции
-%endmacro
-
-%macro read_string 2
-    ; Макрос, предназначенный для чтения строковых сообщений
-    ; Аргументы:
-    ;   1 - Буфер ввода
-    ;   2 - Длина буфера ввода
-    mov     rax, READ       ; системная функция read
-    mov     rdi, STDIN      ; дескриптор файла stdin
-    mov     rsi, %1         ; адрес вводимой строки
-    mov     rdx, %2         ; длина строки
-    syscall                 ; вызов системной функции
-%endmacro
-
 %macro atoi 0
     ; Макрос, предназначенный для перевода строки в число
     ; Входные данные:
@@ -43,20 +19,23 @@
     mov     [rdi], ax           ; [rdi] = ax
 %endmacro
 
-%macro read_integer 3
-    ; Макрос, предназначенный только для чтения 2-байтовых целых
-    ; Аргументы:
-    ;   1 - Буфер ввода
-    ;   2 - Длина буфера ввода
-    ;   3 - Переменная для записи результата преобразований - числа.
-    read_string %1, %2 ; scanf("%s", %1);
-    mov rdi, %3
-    atoi
-%endmacro
+readLn:
+    ; // Пример вызова по C: readLn(char* array, int count);
+    push rbp
+    mov rbp, rsp
+
+    mov     rax, 0          ; системная функция READ
+    mov     rdi, 0          ; дескриптор файла STDIN
+    mov     rsi, [rbp + 16] ; адрес выводимой строки
+    mov     rdx, [rbp + 24] ; длина строки
+    syscall                 ; вызов системной функции
+
+    pop rbp
+    ret 16
 
 numToString:
     ; Входные данные:
-    ;   RSI - адрес строки, завершающейся символом 10
+    ;   RSI - адрес строки
     ;   RCX - размер числа в байтах
     ;   RDI - адрес числа для перевода
     ; Вывод:
@@ -87,6 +66,139 @@ numToString:
     mov     rcx, rax
     ret
 
+print:
+    ; // Пример вызова по C: print(char* array, int count);
+    push rbp
+    mov rbp, rsp
+
+    mov     rax, 1          ; системная функция WRITE
+    mov     rdi, 1          ; дескриптор файла STDOUT
+    mov     rsi, [rbp + 16] ; адрес выводимой строки
+    mov     rdx, [rbp + 24] ; длина строки
+    syscall                 ; вызов системной функции
+
+    pop rbp
+    ret 16
+
+printMatrix:
+    ; // Пример вызова по C: printMatrix(int* array, int rows, int cols);
+    push rbp
+    mov rbp, rsp
+
+    mov rcx, 0
+    ; Цикл вывода строк матрицы
+        cyclePrintRows:
+    push rcx                        ; Положим в стек номер текущей строки
+
+    ; Вывод элементов массива через пробел
+    mov rcx, [rbp + 32]             ; rcx = arg2; // (COLNUM)
+        cyclePrintArr:
+    mov rax, [rsp]                  ; rax = *rsp; // номер текущей строки
+    imul rax, [rbp + 32]            ; rax *= arg2; // кол-во элементов в предыдущих строках
+    add rax, [rbp + 32]             ; rax += arg2; // кол-во элементов в матрице с кол-вом строк, равным текущему номеру строки
+    sub rax, rcx                    ; rax -= rcx; // индекс текущего элемента при нумерации матрицы построчно
+    shl rax, 1                      ; rax *= 2; // смещение в байтах относительно начала массива
+    push rcx                        ; Сохранение счетчика в стек.
+    push 0                          ; char str[8] = {0}; // Объявление локальной переменной
+    mov rsi, rsp                    ; Подготовка
+    mov rcx, 2                      ; к
+    mov rdi, [rbp + 16]             ; вызову
+    add rdi, rax                    ; процедуры
+    call numToString                ; numToString.
+    dec rcx                         ; rcx--; // rcx содержит длину строки с Enter на конце
+    mov rax, rsp                    ; rax = rsp;
+    push rcx                        ; arg1 = rcx;
+    push rax                        ; arg0 = rax;
+    call print                      ; print(arg0, arg1);
+    push lenSpace                   ; arg1 = 1;
+    push SpaceMsg                   ; arg0 = '\t';
+    call print                      ; print(arg0, arg1);
+    add rsp, 8                      ; Забудем переменную str
+    pop rcx                         ; Извлечение счетчика из стека.
+    loop cyclePrintArr
+    push lenEnter                   ; arg1 = 1;
+    push EnterMsg                   ; arg0 = '\n';
+    call print                      ; print(arg0, arg1);
+
+    pop rcx                         ; Извлечем из стека номер текущей строки
+    inc rcx                         ; Переходим на следующую строку
+    cmp rcx, [rbp + 24]
+    jl cyclePrintRows
+    pop rbp
+    ret 24
+
+readMatrix:
+    ; // Пример вызова по C: readMatrix(int* array, int rows, int cols);
+    push rbp
+    mov rbp, rsp
+
+    mov rcx, 0
+    push rcx                        ; int row = rcx;
+    ; Цикл ввода строк матрицы
+        cycleReadRows:
+    mov [rbp - 8], rcx              ; row = rcx;
+
+    mov rcx, 4
+        cycleNewStr:
+    push 0                          ; char* newStr = new char[32];
+    loop cycleNewStr
+
+    push 32
+    mov rax, rbp
+    sub rax, 40
+    push rax
+    call readLn                     ; newStr = gets(); // получим строку, где числа разделены пробелами      
+
+    ; Преобразование строки в одномерный массив чисел
+    mov rcx, 32                     ; rcx = 32;
+        cycleTransformStr:          ; for (int rcx = lenIn; rcx > 0; rcx--) {
+    mov rax, rbp
+    sub rax, 40                     ; rax = newStr;
+    add rax, rcx                    ; rax += rcx;
+    dec rax                         ; rax--;
+    cmp byte [rax], 32              ; if (*rax == ' ') { // заменим все пробелы на Enter
+    jne skipCycleTransformStr
+    mov byte [rax], 10              ; *rax = '\n';
+    mov rsi, rax
+    inc rsi                         ;   Сохраним все адреса замененных пробелов (т.е. адреса строк с числами)
+    push rsi                        ;   в стек, чтобы затем по очереди их извлечь и найти числа для преобразования.
+        skipCycleTransformStr:      ;   }
+    loop cycleTransformStr          ; }
+
+    mov rsi, rbp
+    sub rsi, 40                     ; Адрес первого числа в большой строке известен, но его нет в стеке, поэтому обработаем его отдельно.
+    mov rax, [rbp - 8]              ; rax = row;
+    imul rax, [rbp + 32]            ; rax *= arg2;
+    shl rax, 1                      ; rax *= 2;
+    mov rdi, [rbp + 16]             ; rdi = arg0;
+    add rdi, rax                    ; rdi += rax; // Поместим эффективный адрес в регистр RDI.
+    atoi                            ; Преобразование строки по адресу [RDI] в число типа WORD.
+
+    mov rcx, [rbp + 32]             ; for (int rcx = arg2 - 1; rcx > 0; rcx--) {
+    dec rcx
+        cycleStrToArr:
+    pop rsi                         ;   Берем адреса строк с числами из стека.
+    mov rax, [rbp - 8]              ;   rax = row;
+    imul rax, [rbp + 32]            ;   rax *= arg2;
+    add rax, [rbp + 32]             ;   rax += arg2;
+    sub rax, rcx                    ;   rax -= rax; // В регистр RAX поместим количество элементов для пропуска с начала массива.
+    shl rax, 1                      ;   rax *= 2;
+    mov rdi, [rbp + 16]             ;   rdi = arg0;
+    add rdi, rax                    ;   rdi += rax; // Загрузим в RDI эффективный адрес для результата преобразования.
+    atoi                            ;   Преобразуем.
+    loop cycleStrToArr              ; }
+
+    mov rcx, [rbp - 8]              ; rcx = row;
+    inc rcx                         ; rcx++;
+    add rsp, 32                     ; delete newStr;
+    cmp rcx, [rbp + 24]             ; if (rcx < arg1)
+    jl cycleReadRows                ;   goto cycleReadRows;
+    ; Конец ввода матрицы           ; else
+                                    ;   return;
+    mov rsp, rbp
+    pop rbp
+    ret 24
+
     section .data
 ExitMsg     db      "Press Enter to Exit", 10
 lenExit     equ     $-ExitMsg
@@ -94,7 +206,7 @@ StartMsg    db      "Type matrix 4x6", 10
 lenStart    equ     $-StartMsg
 CheckMsg    db      "Incoming parameters:", 10
 lenCheck    equ     $-CheckMsg
-ResultMsg   db      "The result is"
+ResultMsg   db      "The result is", 10
 lenResult   equ     $-ResultMsg
 SpaceMsg    db      9
 lenSpace    equ     $-SpaceMsg
@@ -102,94 +214,48 @@ EnterMsg    db      10
 lenEnter    equ     $-EnterMsg
 
     section .bss
-InBuf   resb    32  ; буфер ввода
+InBuf   resb    32              ; буфер ввода
 lenIn   equ     $-InBuf
-OutBuf  resb    7   ; буфер вывода
-array   resw    24  ; массив
-row     resq    1   ; счетчик строк массива
+OutBuf  resb    7               ; буфер вывода
+array   resw    ROWNUM*COLNUM   ; массив
 
     section .text
 global _start
 
 _start:
-    write_string StartMsg, lenStart ; puts("Type matrix 4x6");
+    push lenStart
+    push StartMsg
+    call print                      ; puts("Type matrix 4x6");
 
-    mov rcx, 0
-    ; Цикл ввода строк матрицы
-        cycleReadRows:
-    mov [row], rcx
+    push QWORD COLNUM               ; arg2 = COLNUM;
+    push QWORD ROWNUM               ; arg1 = ROWNUM;
+    push array                      ; arg0 = array;
+    call readMatrix                 ; readMatrix(arg0, arg1, arg2);
 
-    read_string InBuf, lenIn        ; InBuf = gets(); // получим строку, где числа разделены пробелами
+    push lenCheck
+    push CheckMsg
+    call print                      ; puts("Incoming parameters:");
 
-    ; Преобразование строки в одномерный массив чисел
-    mov rcx, lenIn                  ; rcx = lenIn
-        cycleTransformStr:          ; for (int rcx = lenIn; rcx > 0; rcx--) {
-    cmp byte [InBuf + rcx - 1], 32  ;   if (InBuf + rcx - 1 == ' ') { // заменим все пробелы на Enter
-    jne skipCycleTransformStr
-    mov byte [InBuf + rcx - 1], 10  ;   *(InBuf + rcx - 1) = '\n';
-    mov rsi, InBuf
-    add rsi, rcx                    ;   Сохраним все адреса замененных пробелов (т.е. адреса строк с числами)
-    push rsi                        ;   в стек, чтобы затем по очереди их извлечь и найти числа для преобразования.
-        skipCycleTransformStr:      ;   }
-    loop cycleTransformStr          ; }
+    push QWORD COLNUM               ; arg2 = COLNUM;
+    push QWORD ROWNUM               ; arg1 = ROWNUM;
+    push array                      ; arg0 = array;
+    call printMatrix                ; printMatrix(arg0, arg1, arg2);
 
-    mov rsi, InBuf                  ; Адрес первого числа в большой строке известен, но его нет в стеке,
-    mov rax, [row]                  ; поэтому обработаем его отдельно.
-    imul rax, COLNUM                ; В регистр RAX поместим количество элементов для пропуска с начала массива.
-    lea rdi, [rax * 2 + array]      ; Поместим эффективный адрес в регистр RDI.
-    atoi                            ; Преобразование строки по адресу [RDI] в число типа WORD.
-
-    mov rcx, COLNUM - 1             ; for (int rcx = COLNUM - 1; rcx > 0; rcx--) {
-        cycleStrToArr:
-    pop rsi                         ;   Берем адреса строк с числами из стека.
-    mov rax, [row]
-    imul rax, COLNUM
-    add rax, COLNUM                 ;   Подсчитаем эффективный адрес для сохранения числа во внутреннем представлении.
-    sub rax, rcx                    ;   В регистр RAX поместим количество элементов для пропуска с начала массива.
-    lea rdi, [rax * 2 + array]      ;   Загрузим в RDI эффективный адрес для результата преобразования.
-    atoi                            ;   Преобразуем.
-    loop cycleStrToArr              ; }
-
-    mov rcx, [row]
-    inc rcx
-    cmp rcx, ROWNUM
-    jl cycleReadRows
-    ; Конец ввода матрицы
-
-    write_string CheckMsg, lenCheck ; puts("Incoming parameters:");
-    mov rcx, 0
-    ; Цикл вывода строк матрицы
-        cyclePrintRows:
-    mov [row], rcx
-
-    ; Вывод элементов массива через пробел
-    mov rcx, COLNUM                 ; rcx = COLNUM;
-        cyclePrintArr:
-    mov rax, [row]                  ; В регистр RAX поместим количество элементов для пропуска с начала массива.
-    imul rax, COLNUM
-    add rax, COLNUM
-    sub rax, rcx
-    push rcx                        ; Сохранение счетчика в стек.
-    mov rsi, OutBuf                 ; Подготовка
-    mov rcx, 2                      ; к вызову
-    lea rdi, [rax * 2 + array]      ; процедуры
-    call numToString                ; numToString.
-    dec rcx
-    write_string OutBuf, rcx        ; printf("%d ", d);
-    write_string SpaceMsg, lenSpace
-    pop rcx                         ; Извлечение счетчика из стека.
-    loop cyclePrintArr
-    write_string EnterMsg, lenEnter
-
-    mov rcx, [row]
-    inc rcx
-    cmp rcx, ROWNUM
-    jl cyclePrintRows
-    ; Конец вывода матрицы
+    push lenResult
+    push ResultMsg
+    call print                      ; puts("The result is");
+    push QWORD COLNUM               ; arg2 = COLNUM;
+    push QWORD ROWNUM               ; arg1 = ROWNUM;
+    push array                      ; arg0 = array;
+    call printMatrix                ; printMatrix(arg0, arg1, arg2);
 
     ; Завершение программы
-    write_string ExitMsg, lenExit   ; puts("Press Enter to Exit");
-    read_string InBuf, lenIn        ; gets();
+    push lenExit
+    push ExitMsg
+    call print                      ; puts("Press Enter to Exit"); 
+    push lenIn
+    push InBuf
+    call readLn                     ; gets();     
     mov     rax, 60                 ; системная функция 60 (exit)
     xor     rdi, rdi                ; return code 0    
     syscall                         ; вызов системной функции
